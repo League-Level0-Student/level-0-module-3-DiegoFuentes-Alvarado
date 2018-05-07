675b37a21bd12cb06650e4e2988a6d409dab57fb
Student_2 <Student_2@165.24.36.199>
2018-05-07 15:59:19 -0700

    Birthday reminder, crazy cat lady, high low game

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -16,15 +16,27 @@ public class BirthdayReminder {
 		String myBirthday = "April 16th";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+		int birthday = JOptionPane.showOptionDialog(null, "Who's birthday?", "Tittle", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Mom", "Dad", "Me" }, null);
+		System.out.println();
 		// 3. Print out what the user typed
 		
 		// 4. if user asked for "mom"
 			//print mom's birthday
+		if(birthday == 0){
+			System.out.println("Mom " + momsBirthday);
+		}
 		// 5. if user asked for "dad"
 			// print dad's birthday
+		if(birthday == 1){
+			
+			System.out.println("Dad " + dadsBirthday);
+		}
 		// 6. if user asked for your name
 			// print myBirthday
+		if(birthday == 2){
+			System.out.println("Me " + myBirthday);
+		}
 		//7. otherwise print "Sorry, i don't remember that person's birthday!"
 
 	} 
--- src/elseif/CrazyCatLady.java
+++ src/elseif/CrazyCatLady.java
@@ -5,18 +5,27 @@ package elseif;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		// 1. Ask the user how many cats they have
-
+		String cats = JOptionPane.showInputDialog("How many cats do you hove?" );
+			int c =	Integer.parseInt(cats);
 		// 2. Convert their answer into an int
 
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
-
+			if(c >= 3){
+				System.out.println("You're a crazy cat person!");
+			}
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
-
+			if(c < 3 && c > 0){
+				playVideo("https://www.youtube.com/watch?v=5dsGWM5XGdg");
+			}
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
-		
+			if(c == 0){
+				playVideo("https://www.youtube.com/watch?v=oj_yLBltPE8");
+			}
 	}
 
 	static void playVideo(String videoURL) {
